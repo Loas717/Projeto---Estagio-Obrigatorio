a92038eab965fe71dbe1e4ce52e432aaf0cbaccd
Loas717 <133361796+Loas717@users.noreply.github.com>
2026-03-27 19:17:51 -0300

    change

--- routes/certificados.js
+++ routes/certificados.js
@@ -3,15 +3,13 @@ const router = express.Router();
 const multer = require('multer');
 const { registrar } = require('../controllers/certificadoController');
 
-// Configuração do multer para upload de arquivos
 const upload = multer({ 
     dest: 'uploads/',
     limits: {
-        fileSize: 10 * 1024 * 1024 // 10MB
+        fileSize: 10 * 1024 * 1024 //10MB
     }
 });
 
-// Rota para registrar um novo certificado
 router.post('/registrar', upload.single('arquivo'), registrar);
 
 module.exports = router;
