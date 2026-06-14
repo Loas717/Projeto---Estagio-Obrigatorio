b71bb9bb345470ec61c9bb9108125ae671da3291
Loas717 <133361796+Loas717@users.noreply.github.com>
2026-06-14 17:39:12 -0300

    ipfs fix

--- services/ipfsService.js
+++ services/ipfsService.js
@@ -1,24 +1,28 @@
 const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
 require('dotenv').config();
 
-async function uploadToIPFS(jsonDocument) {
+async function uploadToIPFS(caminhoDoArquivo) {
     try {
+        const data = new FormData();
+        data.append('file', fs.createReadStream(caminhoDoArquivo));
         const config = {
             method: 'post',
-            url: 'https://api.pinata.cloud/pinning/pinJSONToIPFS',
+            url: 'https://api.pinata.cloud/pinning/pinFileToIPFS',
             headers: { 
-                'Content-Type': 'application/json',
+                ...data.getHeaders(),
                 'Authorization': `Bearer ${process.env.PINATA_JWT}`
             },
-            data: jsonDocument
+            data: data
         };
 
         const res = await axios(config);
         
-        console.log("Arquivo no IPFS com CID:", res.data.IpfsHash);
+        console.log("PDF enviado com sucesso para o IPFS com CID:", res.data.IpfsHash);
         return res.data.IpfsHash; 
     } catch (error) {
-        console.error("Erro ao subir para IPFS:", error);
+        console.error("Erro ao subir PDF para IPFS:", error);
         throw error;
     }
 }
